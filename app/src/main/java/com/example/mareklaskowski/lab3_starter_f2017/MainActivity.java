64d598e73321075415e52b1b22036979d8298ba2
Marek Laskowski <marek.laskowski@senecacollege.ca>
2017-09-26 08:48:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mareklaskowski/lab3_starter_f2017/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mareklaskowski.lab3_starter_f2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
